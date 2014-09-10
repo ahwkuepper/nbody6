866ab90847d7a6dfd7f67a4a46a943938eb31710
Andreas H.W. Küpper <ahwkuepper@gmail.com>
2014-09-10 17:32:04 -0400

    clean up

--- Ncode/define.f
+++ Ncode/define.f
@@ -43,7 +43,7 @@
 *       GMIN    Relative two-body perturbation for unperturbed motion.
 *       GMAX    Secondary termination parameter for soft KS binaries.
 ***
-* INPUT: if (kz(4).gt.0)
+* INPUT: if (kz(4).gt.0) (suppressed)
 *
 *       DELTAS  Output interval for binary search (in TCR; suppressed).
 *       ORBITS  Minimum periods for binary output (level 1).
@@ -69,9 +69,8 @@
 *
 *        if (kz(5).eq.3)
 *
-*       APO     Separation between the perturber and Sun.
-*       ECC     Eccentricity of orbit (=1 for parabolic encounter).
-*       DMIN    Minimum distance of approach (pericentre).
+*       APO     Initial apocentre distance from the Sun (N-body units).
+*       ECC     Eccentricity of orbit (assumed < 0.90).
 *       SCALE   Perturber mass scale factor (=1 for Msun).
 *
 *        if (kz(5).eq.4)
@@ -166,7 +165,7 @@
 *       CLM     Individual cloud masses in solar masses (maximum MCL).
 *       RCL2    Half-mass radii of clouds in pc (square is saved).
 ***
-* CHAIN: if (kz(11).gt.0 with ARChain)
+* CHAIN: if compiled with ARChain
 *
 *       CLIGHT  Velocity of light in N-body units (e.g. 3.0D+05/VSTAR).
 *       NBH     Number of BHs for special treatment (redundant but keep).
@@ -181,9 +180,9 @@
 *       1  COMMON save unit 1 (=1: 'touch STOP'; =2: every 100*NMAX steps).
 *       2  COMMON save unit 2 (=1: at output; =2: restart if DE/E > 5*QE).
 *       3  Basic data unit 3 at output time (unformatted, frequency NFIX;
-*             =1/2: standard /and tail; =3: tail only; >3: cluster + tail).
+*             =1/2: standard and tail; =3: tail only; >3: cluster + tail).
 *       4  Binary diagnostics on unit 4 (# threshold levels = KZ(4) < 10);
-*                                       (currently suppressed in ksint.f).
+*                                       (suppressed in all routines).
 *       5  Initial conditions (#22 =0; =0: uniform & isotropic sphere);
 *                =1: Plummer; =2: two Plummer models in orbit, extra input;
 *                =3: massive perturber and planetesimal disk, extra input;
@@ -194,7 +193,7 @@
 *                =1: soft & regularized binaries on unit 6;
 *                =2: regularized binaries only;
 *                >2: individual bodies (loop from 1 to KZ(6)).
-*       7  Lagrangian radii (>0: RSCALE; =2, 3, 4: output units 6, 7, 12);
+*       7  Lagrangian radii (>0: RSCALE; =2, 3, 4: output units 6, 7);
 *                >=2: half-mass radii of 50% mass, also 1% heavies, unit 6;
 *                >=2: Lagrangian radii for two mass groups on unit 31 & 32;
 *                >=2: geometric radii for three mass groups on unit 6;
@@ -251,7 +250,7 @@
 *                         =4 and #27 = 3: neutron star capture (instar.f).
 *      29  Boundary reflection for hot system (suppressed).
 *      30  Multiple regularization (=1: all; >1: BEGIN/END; >2: each step);
-*                                =-1: CHAIN only; =-2: TRIPLE & QUAD only. 
+*                                               =-1: CHAIN only for BH KS.
 *      31  Centre of mass correction after ADJUST (don't use with #23 = 0).
 *      32  Increase output intervals & SMAX based on single particle energy.
 *      33  Histograms at main output (>=1: STEP; =2: STEPR, NBHIST & BINARY).
@@ -352,6 +351,7 @@
 *       NSN     Neutron stars.
 *       NBH     Black holes.
 *       NBS     Blue stragglers.
+*       NTZ     Thorne-Zytkow objects.
 *       ---------------------------------------------------------------------
 *
 *
@@ -375,10 +375,21 @@
 *      13       Neutron star.
 *      14       Black hole.
 *      15       Massless supernova remnant.
-*      19       Circularizing binary (c.m. value).
-*      20       Circularized binary.
-*      21       First Roche stage (inactive).
-*      22       Second Roche stage.
+*       ---------------------------------------------------------------------
+*
+*       Binary types
+*       ************
+*
+*       ---------------------------------------------------------------------
+*       0       Standard case.
+*      -1       Chaotic (option 27 = 2).
+*      -2       Continuous circularizing (option 27 = 2).
+*       9       Sequential circularization (option 27 = 1).
+*      10       Circularized.
+*      11       First Roche stage (option 34 = 1/2).
+*      12       End of first Roche stage.
+*      13       Start of second Roche stage.
+*      xx       Further Roche stages.
 *       ---------------------------------------------------------------------
 *
       RETURN
